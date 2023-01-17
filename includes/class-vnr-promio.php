bb9192ae2de2bda85dfea7f6a589116cd939fd32
DESKTOP-K4P3EBG\peter <peter.mertzlin@gmail.com>
2023-01-17 12:35:44 +0100

    removed old classes and replaced with new added result output to localStorage

--- includes/class-vnr-promio.php
+++ includes/class-vnr-promio.php
@@ -28,15 +28,206 @@
  * @author     Steve Kraft & Peter Mertzlin <direct@extensionforge.com>
  */
 
+/**
+	 * @var string Holds the http url targeting the api endpoint
+	 */
+
+function subscribe($email_address, $newsletter_abbreviations, $opt_in_process_id, $affiliate,$trader_fox, $bullvestor, $coreg, $adref,
+									 $additional_properties) {
+
+		$customer_remote_address = null;
+		if (isset($_SERVER['HTTP_X_REAL_IP']) && strlen(trim($_SERVER['HTTP_X_REAL_IP'])) > 0) {
+			$customer_remote_address = $_SERVER['HTTP_X_REAL_IP'];
+		} elseif (isset($_SERVER['REMOTE_ADDR']) && strlen(trim($_SERVER['REMOTE_ADDR'])) > 0) {
+			$customer_remote_address = $_SERVER['REMOTE_ADDR'];
+		}
+
+		$calling_service = sprintf(
+			'%s://%s%s',
+			isset($_SERVER['HTTPS']) ? 'https' : 'http',
+			isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '',
+			isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : ''
+		);
+
+		$user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null;
+
+		$visitor_id = isset($_COOKIE['ePPxlID']) ? $_COOKIE['ePPxlID'] : null;
+
+		$session_id = isset($_COOKIE['ePxlID']) ? $_COOKIE['ePxlID'] : null;
+
+		$response = remote_call(
+			'Subscribe',
+			[
+				$email_address, $newsletter_abbreviations, $opt_in_process_id, $customer_remote_address,
+				$calling_service, $affiliate, $visitor_id, $session_id, $trader_fox, $bullvestor, $coreg, $user_agent,
+				$adref, $additional_properties
+			]
+		);
+
+		if (!is_array($response)) {
+			throw new Exception('invalid response format');
+		}
+
+		// relocate to soi page if provided
+		if (isset($response[0])) {
+			if ($callback !== null && is_callable($callback)) {
+				$callback();
+			}
+			relocate($response[0]);
+		}
+
+	}
+
+
+
+	function relocate($uri){
+		header(sprintf('Location: %s', $uri));
+		exit();
+	}
+
+	
+
+	
+	/**
+	 * @param array $parameters
+	 * @return mixed
+	 * @throws Exception
+	 */
+	function remote_call($method, $parameters = []) {
+
+		 $API_ENDPOINT_COMPUTERWISSEN = 'https://nss-computerwissen.de/api';
+		 $API_PASSWORD = 'iiJnaOixQqVnyqGwHXKG';
+
+		$request = new stdClass();
+		$request->Method = $method;
+		$request->Parameters = $parameters;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $API_ENDPOINT_COMPUTERWISSEN);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$raw_response = curl_exec($ch);
+		if (curl_errno($ch) > 0) {
+			throw new Exception(curl_error($ch));
+		}
+		curl_close($ch);
+
+		$response = json_decode($raw_response);
+		if ($response === null) {
+			file_put_contents(
+				__DIR__ . DIRECTORY_SEPARATOR . 'nss_api_errors.txt',
+				sprintf("======== %s ========\r\n%s\r\n================", strftime('%Y-%m-%d %H:%M:%S'), $raw_response),
+				FILE_APPEND
+			);
+			throw new Exception('response json cannot be decoded');
+		}
+
+		if (count($response->Errors) > 0) {
+			$error_message = '';
+			foreach($response->Errors as $error) {
+				if (!is_object($error)) {
+					$error_message .= 'invalid error format';
+				} else {
+					if (!isset($error->Message) || strlen(trim($error->Message)) === 0) {
+						$error_message .= 'unknown error';
+					} else {
+						$error_message .= $error->Message;
+					}
+				}
+				$error_message .= ', ';
+			}
+			throw new Exception(trim($error_message, ' ,'));
+		}
+
+		if (!isset($response->Result)) {
+			throw new Exception('invalid response format');
+		}
+
+		return $response->Result;
+
+	}
+
 
 
 add_action('set_logged_in_cookie', 'custom_get_logged_in_cookie_vnrpromio', 10, 6);
 function custom_get_logged_in_cookie_vnrpromio($logged_in_cookie, $expire, $expiration, $user_id, $logged_in_text, $token)
 {
 		global $wpdb;
-	    $current_user = wp_get_current_user();
+	    $current_user = get_user_by( 'id', $user_id ); 
     	$emailuser = $current_user->user_email;
-		//update_user_meta( get_current_user_id(), 'promio_nl_send', true );	   
+
+		$anredex     = "H";
+		$vorname     = "unbekannt";
+		$nachname     = "unbekannt";
+		
+		if(null !== xprofile_get_field_data( "Interessen", $user_id, 'comma' )){
+			$interessenx = xprofile_get_field_data( "Interessen", $user_id, 'comma' );	
+		}
+		if(null !== xprofile_get_field_data( "Anrede", $user_id, '' )){$anredex     = xprofile_get_field_data( "Anrede", $user_id, '' );}
+		if(null !== xprofile_get_field_data( "Vorname", $user_id, '' )){$vorname     = xprofile_get_field_data( "Vorname", $user_id, '' );}
+		if(null !== xprofile_get_field_data( "Nachname", $user_id, '' )){$nachname     = xprofile_get_field_data( "Nachname", $user_id, '' );}
+		
+		
+		$triggered = false;
+		$triggered = get_user_meta($user_id,"promio_nl_send");
+
+		if ($anredex=='Frau') { $anrede = "F";} else {$anrede = "H";}
+		
+		//xprofile_get_field_data
+		//var_dump($interessen);
+		//echo $anrede." ".$vorname." ".$nachname." ".$email;
+		//var_dump($interessen);
+		$interessen = "CWC, ".$interessenx;
+		if($interessen){
+		
+		$abos = array_map('trim', explode(",",$interessen));
+
+		//delete to activate
+		//$triggered=false;
+		//$testo = $wpdb->insert("tester", array('text' => json_encode($triggered)), array('%s') );
+		//$testo = $wpdb->insert("tester", array('text' => json_encode($emailuser)), array('%s') );
+		//$testo = $wpdb->insert("tester", array('text' => json_encode($interessen)), array('%s') );
+		//$testo = $wpdb->insert("tester", array('text' => json_encode($vorname)), array('%s') );
+		//$testo = $wpdb->insert("tester", array('text' => json_encode($nachname)), array('%s') );
+		//$testo = $wpdb->insert("tester", array('text' => json_encode($anrede)), array('%s') );
+		
+		if($triggered==false){
+			// do submit
+			 try {
+				subscribe(
+				$emailuser,
+				$abos,
+				'114',
+				'SEO_CW_CWC_WEB_OA_computerwissen-club',
+				false,
+				false,
+				'',
+				null,
+				json_decode(json_encode([
+					'attributeKey[0]' => 'FIRST_NAME',
+					'attributeValue[0]' => $vorname,
+					'attributeKey[1]' => 'LAST_NAME',
+					'attributeValue[1]' => $nachname,
+					'attributeKey[2]' => 'ANREDE',
+					'attributeValue[2]' => $anrede,
+					'immediateConfirmation' => 'PCemupZnsudHNWDeHd3CU2TbPVQWHpF3'
+				]))
+					);
+	localStorage.setItem("NssApiError", "No Error - so far!");
+				} catch (Exception $exception) {
+					//echo 'Fehler: API!<br />';
+					//$testo = $wpdb->insert("tester", array('text' => json_encode($exception->getMessage())), array('%s') );
+					//print($exception->getMessage());
+	localStorage.setItem("NssApiError", $exception->getMessage());
+				}          
+
+		}
+
+		}
+
+		update_user_meta( get_current_user_id(), 'promio_nl_send', true );	   
 }
 
 
@@ -117,7 +308,7 @@ class Vnr_Promio {
 		 * core plugin.
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-vnr-promio-loader.php';
-		//require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/NssApi.class.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/NssApi.class.php';
 
 		/**
 		 * The class responsible for defining internationalization functionality
